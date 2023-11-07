3b9c2c6f63d63937a439d45dd0c4a2b655470a6d
Andrii Marchenko <136458614+on-sedatives@users.noreply.github.com>
2023-11-07 14:49:07 +0100

    Delete dentist - Copy directory

--- dentist - Copy/dentist/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from dentist import models
-
-admin.site.register(models.Team)
-admin.site.register(models.Service)
-admin.site.register(models.Staff)
-admin.site.register(models.Specialization)
-admin.site.register(models.Page)
